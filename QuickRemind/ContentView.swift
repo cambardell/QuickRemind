83fdd90b582019d68a3b1bcba4dc93dc83ede461
Cameron Bardell <cambardell3@gmail.com>
2019-07-04 15:52:22 -0400

    Initial Commit

---
+++ QuickRemind/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  QuickRemind
+//
+//  Created by Cameron Bardell on 2019-07-04.
+//  Copyright © 2019 Cameron Bardell. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
