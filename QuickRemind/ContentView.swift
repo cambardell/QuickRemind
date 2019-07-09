3bd9ac6ba89fa5273134c284424735fe84633423
Cameron Bardell <cambardell3@gmail.com>
2019-07-09 18:22:30 -0400

    Fixed eventkit access request bug

--- QuickRemind/ContentView.swift
+++ QuickRemind/ContentView.swift
@@ -7,12 +7,100 @@
 //
 
 import SwiftUI
+import EventKit
 
 struct ContentView : View {
+    
+    @State var reminderText: String = ""
+    @State var reminderDate: Date = Date()
+
+    var eventStore = EKEventStore()
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Spacer()
+            Text("Remind me to \(reminderText) on \(reminderDate)")
+                .lineLimit(nil)
+                .padding()
+            
+            DatePicker($reminderDate)
+           
+            HStack{
+                addTimeButton(reminderDate: reminderDate, timeToAdd: "Hour")
+                addTimeButton(reminderDate: reminderDate, timeToAdd: "Day")
+                addTimeButton(reminderDate: reminderDate, timeToAdd: "Week")
+                Spacer()
+            }.padding()
+            
+            TextField("Remind me to...", text: $reminderText)
+                .padding()
+                .lineLimit(nil)
+            
+
+            withAnimation{
+                Button(action: {
+                    self.saveReminder()
+                    
+                }) {
+                    Text("Save")
+                        .color(.black)
+                    
+                }
+                .padding()
+                    .background(Color.orange)
+                    .cornerRadius(20)
+            }
+            
+            
+           Spacer()
+        }
+       
     }
+    func saveReminder() {
+        
+        let reminder = EKReminder(eventStore: self.eventStore)
+        let components = Calendar.current.dateComponents([Calendar.Component.minute, Calendar.Component.hour, Calendar.Component.day, Calendar.Component.month, Calendar.Component.year], from: reminderDate)
+        reminder.title = reminderText
+        reminder.calendar = eventStore.defaultCalendarForNewReminders()
+        reminder.dueDateComponents = components
+        
+      
+        do {
+            try eventStore.save(reminder,
+                                commit: true)
+        } catch let error {
+            print("Reminder failed with error \(error.localizedDescription)")
+        }
+        withAnimation(.spring()){
+            reminderText = ""
+        }
+
+        reminderDate = Date()
+    }
+
 }
+
+
+struct addTimeButton : View {
+    var reminderDate: Date
+    var timeToAdd: String
+    
+    var body: some View {
+        return Button(action: {
+    
+        }) {
+            Text("Add \(timeToAdd)").color(.black)
+            }
+            .padding()
+            .background(Color(red: 99/255, green: 193/255, blue: 178/255, opacity: 1.0))
+            .cornerRadius(20)
+    }
+    
+   
+}
+
+
+
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
@@ -21,3 +109,4 @@ struct ContentView_Previews : PreviewProvider {
     }
 }
 #endif
+
